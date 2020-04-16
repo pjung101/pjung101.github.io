a0acf62a96f713434527a8178a235a69f75ef64a
Pete Jung <pete.y.jung@gmail.com>
2020-04-15 22:58:34 -0700

    first commit

---
+++ precache-manifest.8101a53018169306d897b63bb886741c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c0656c001d2c3489043c4402e43f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2de157eb22277eb326d6",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9ce53f071df24fe1f4dc",
+    "url": "/static/css/main.fdfe7409.chunk.css"
+  },
+  {
+    "revision": "2de157eb22277eb326d6",
+    "url": "/static/js/2.f61ec32e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f61ec32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce53f071df24fe1f4dc",
+    "url": "/static/js/main.43a7da80.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
